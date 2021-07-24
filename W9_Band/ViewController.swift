2214d3ba071b84128f51fac1eedd390e5b96f795
“Arisa <arisa.yachun@gmail.com>
2021-07-24 21:44:00 +0800

    all

--- W9_Band/ViewController.swift
+++ W9_Band/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let backBarButtton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem = backBarButtton
     }
 
 
